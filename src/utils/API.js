e5fd46a7e81b3183e4469a1be3a668185236e034
Isabel <iarcones@gmail.com>
2020-08-10 14:23:01 -0700

    test

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,18 +3,18 @@ import axios from "axios";
 export default {
   // Gets books from the Google API
   getBooks: function(q) {
-    return axios.get("/api/google", { params: { q: "title:" + q } });
+    return axios.get("https://googlebooks-server.herokuapp.com/api/google", { params: { q: "title:" + q } });
   },
   // Gets all saved books
   getSavedBooks: function() {
-    return axios.get("/api/books");
+    return axios.get("https://googlebooks-server.herokuapp.com/api/books");
   },
   // Deletes the saved book with the given id
   deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+    return axios.delete("https://googlebooks-server.herokuapp.com/api/books/" + id);
   },
   // Saves an book to the database
   saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+    return axios.post("https://googlebooks-server.herokuapp.com/api/books", bookData);
   }
 };
